f7c9735c6ef73d6d7664a0800ca6c3ae36a2fa09
LucyIsCute <fietek06@gmail.com>
2023-12-17 14:19:07 +0100

    first commit!!!

---
+++ .dwm/autostart.sh
@@ -0,0 +1,5 @@
+exec picom --backend glx --blur-method dual_kawase --vsync &
+     vencord-desktop &
+     steam &
+     /home/lucy/.fehbg &
+     dwmblocks &
---
+++ .dwm/spot_playing.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+playerctl metadata xesam:title && echo "  -  " && playerctl metadata xesam:artist
